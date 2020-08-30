fbcf16d91b9b400e62a3ae710b2dd84e86fb7bf1
zanettanorris <60621610+zanettanorris@users.noreply.github.com>
2020-08-30 00:38:35 -0400

    Added code to problem statement

--- CurrencyFormatter.java
+++ CurrencyFormatter.java
@@ -33,3 +33,32 @@
 
 // Explanation
 // Each line contains the value of payment formatted according to the four countries' respective currencies.
+
+import java.util.*;
+import java.text.*;
+
+public class Solution {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+        
+        // Write your code here.
+        NumberFormat u = NumberFormat.getCurrencyInstance();
+        NumberFormat i = NumberFormat.getCurrencyInstance(new Locale("en", "in"));
+        NumberFormat c = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        NumberFormat f = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        
+        String us = u.format(payment);
+        String india = i.format(payment);
+        String china = c.format(payment);
+        String france = f.format(payment);
+        
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
+
